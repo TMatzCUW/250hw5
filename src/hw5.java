a45532df5e34b265179efdd01fa61e137aaa9b95
TMatzCUW <70924911+TMatzCUW@users.noreply.github.com>
2020-09-21 22:15:31 -0500

    Finished

--- src/hw5.java
+++ src/hw5.java
@@ -4,5 +4,9 @@ public class hw5 {
 	public static void main(String[] args) {
 		Biblebook m=new Biblebook("Mark", 16, "The Second Gospel of Jesus");
 		m.display();
+		Biblebook t=new Biblebook("1 Timothy", 6, "The First Letter from Paul to Tim");
+		t.display();
+		Biblebook r=new Biblebook("Revelation", 22, "The End of the World");
+		r.display();
 	}
 }
