8f67b41da97759523672975cbc8e9597e026c8ca
TMatzCUW <70924911+TMatzCUW@users.noreply.github.com>
2020-09-21 22:12:09 -0500

    Working

---
+++ src/Biblebook.java
@@ -0,0 +1,18 @@
+
+public class Biblebook {
+	
+		public String name;
+		public int chapter;
+		public String desc;
+		
+		public Biblebook(String name, int chapter, String desc){
+			this.name=name;
+			this.chapter=chapter;
+			this.desc=desc;
+		}
+		
+		public void display() {
+			System.out.println(this.name+" has "+this.chapter+" chapters and is "+this.desc+".");
+		}
+	}
+
--- src/hw5.java
+++ src/hw5.java
@@ -1,4 +1,8 @@
 
 public class hw5 {
-
+//Bible book: Give name, chapters, and brief description. Object
+	public static void main(String[] args) {
+		Biblebook m=new Biblebook("Mark", 16, "The Second Gospel of Jesus");
+		m.display();
+	}
 }
